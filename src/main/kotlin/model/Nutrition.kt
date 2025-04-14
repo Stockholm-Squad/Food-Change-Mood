4652c457ad30640a2eb254b0ff2775ff2aa76af0
alaa noori <alaanoori.prog@gmail.com>
2025-04-14 20:31:51 +0300

    add structure for project

---
+++ src/main/kotlin/model/Nutrition.kt
@@ -0,0 +1,11 @@
+package org.example.model
+
+data class Nutrition (
+    val calories:Float=0f,
+    val totalFat:Float=0f,
+    val sugar:Float=0f,
+    val sodium:Float=0f,
+    val protein:Float=0f,
+    val saturatedFat:Float=0f,
+    val carbohydrates:Float=0f,
+)
