e57fc9d3f2c54f686acd8ac5398ac80641ca6b7e
Mona <monaayman584@gmail.com>
2025-04-15 22:07:35 +0200

    add get meal

--- src/main/kotlin/logic/GetMealsByName.kt
+++ src/main/kotlin/logic/GetMealsByName.kt
@@ -1,4 +1,13 @@
 package org.example.logic
 
-class GetMealsByName {
+import model.Meal
+import org.example.data.CsvMealsRepository
+
+class GetMealsByName(
+    private val mealsRepository: CsvMealsRepository
+) {
+    fun getMealsByName(name:String):List<Meal>{
+      val allMeals=  mealsRepository.getAllMeals()
+      return  searchMealsByNameKMP(allMeals,name)
+    }
 }
