3da7b5468172624b1b1abeb94828e4c86361cb22
Thoraya <thoriahamdy99@gmail.com>
2025-04-16 22:57:41 +0200

    - change meal date to be Date instead of string

--- src/main/kotlin/model/Meal.kt
+++ src/main/kotlin/model/Meal.kt
@@ -1,12 +1,14 @@
 package model
 
+import java.util.Date
+
 
 data class Meal(
     val name: String?,
     val id: Int, // Non-nullable because every meal must have an ID
     val minutes: Int?,
     val contributorId: Int, // Non-nullable because every meal has a contributor
-    val submitted: String?,
+    val submitted: Date?,
     val tags: List<String>?,
     val nutrition: Nutrition, // Non-nullable but with default values
     val numberOfSteps: Int?,
