ab2625acc3de5caa64795f0275699679da7b4246
alaa noori <alaanoori.prog@gmail.com>
2025-04-19 00:44:26 +0300

    update get iraqi males use case implementation - return list of meals - remove redundant filter

--- src/main/kotlin/logic/GetIraqiMealsUseCase.kt
+++ src/main/kotlin/logic/GetIraqiMealsUseCase.kt
@@ -7,23 +7,15 @@ class GetIraqiMealsUseCase(
     private val mealRepository: MealsRepository,
 ) {
     //TODO handle it to return List<Meal> and handle 2 filters
-    fun getIraqiMales(): List<Triple<String?, Int?, String?>> {
+    fun getIraqiMales(): List<Meal> {
         return mealRepository.getAllMeals()
             .takeIf { meals ->
                 meals.isNotEmpty()
             }
-            ?.filter(::onlyNotEmptyData)
             ?.filter { meal ->
                 meal.description?.contains("iraq", ignoreCase = true) == true || meal.tags?.contains("iraqi") == true
-            }
-            ?.map { iraqiMeal ->
-                Triple(iraqiMeal.name, iraqiMeal.minutes, iraqiMeal.description)
             } ?: throw IllegalStateException("Sorry, we don't have any iraqi meals available")
 
-    }
-
-    private fun onlyNotEmptyData(input: Meal): Boolean {
-        return input.description.toString().isNotBlank() || input.tags?.size != 0
     }
 
 
