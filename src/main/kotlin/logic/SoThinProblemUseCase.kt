fa91c759980157e0320ec70dae48f856843e4e0d
alaa noori <alaanoori.prog@gmail.com>
2025-04-19 02:28:12 +0300

    update get so thin problem use case - rename the file - return meal

---
+++ src/main/kotlin/logic/SoThinProblemUseCase.kt
@@ -0,0 +1,29 @@
+package org.example.logic
+
+import model.Meal
+import model.Nutrition
+
+//TODO rename the useCase
+class SoThinProblemUseCase(
+    private val mealRepository: MealsRepository,
+) {
+    //TODO handle the return type to be Meal
+    fun suggestMealToSoThinPeople(): Meal {
+        return mealRepository.getAllMeals()
+            .takeIf { meals ->
+                meals.isNotEmpty()
+            }
+            ?.filter { meal ->
+                        meal.nutrition.hasMoreThanMinCalories()
+            }?.random()
+            ?: throw IllegalStateException("Sorry, No meal found with more than 700 calories")
+    }
+
+    private fun Nutrition.hasMoreThanMinCalories(): Boolean {
+        return this.calories > MIN_CALORIES
+    }
+
+    companion object {
+        private const val MIN_CALORIES = 700
+    }
+}
