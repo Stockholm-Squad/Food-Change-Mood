1734924d303e8b76648a090ee5a08058efc80876
Abdelrahman Rashed <abdelrahman.rashed.ali@gmail.com>
2025-04-19 13:20:35 +0200

    refactor keto diet use case after review

--- src/main/kotlin/logic/usecases/KetoDietUseCase.kt
+++ src/main/kotlin/logic/usecases/KetoDietUseCase.kt
@@ -7,24 +7,13 @@ class KetoDietUseCase(private val mealRepository: MealsRepository) {
 
     private val suggestedMeals = mutableSetOf<Int>()
 
-    //TODO handle renaming of this function
-    //TODO handle it to be functional programming
-    fun getNextKetoMeal(): Meal? {
-        val allMeals = mealRepository.getAllMeals()
-
-        val ketoMeals = allMeals.filter { meal ->
-            meal.nutrition.carbohydrates < 10 && meal.nutrition.totalFat > 15 && meal.nutrition.protein in 10f..30f
-        }.filterNot { meal ->
-            suggestedMeals.contains(meal.id)
-        }
-
-        if (ketoMeals.isEmpty()) {
-            return null
-        }
-
-        val nextMeal = ketoMeals.random()
-        suggestedMeals.add(nextMeal.id)
-
-        return nextMeal
+    fun getKetoMeal(): Meal? {
+        return mealRepository.getAllMeals()
+            .asSequence()
+            .filter { it.nutrition.carbohydrates < 10 && it.nutrition.totalFat > 15 && it.nutrition.protein in 10f..30f }
+            .filterNot { suggestedMeals.contains(it.id) }
+            .shuffled()
+            .firstOrNull()
+            ?.also { suggestedMeals.add(it.id) }
     }
 }
