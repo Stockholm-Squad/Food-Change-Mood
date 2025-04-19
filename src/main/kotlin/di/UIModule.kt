1bed7c969c8bbd542b3c9ef2f4226c7a78f01f8f
Thoraya <thoriahamdy99@gmail.com>
2025-04-19 16:46:43 +0200

    - handle ui injection by another way.

--- src/main/kotlin/di/UIModule.kt
+++ src/main/kotlin/di/UIModule.kt
@@ -1,26 +1,9 @@
 package org.example.di
 
 import org.example.presentation.FoodConsoleUI
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val uiModule = module {
-    single<FoodConsoleUI> {
-        FoodConsoleUI(
-            getHealthFastFoodUseCase = get(),
-            getEasyFoodSuggestionsUseCase = get(),
-            sweetNoEggsUseCase = get(),
-            getCountryFoodUseCase = get(),
-            getPotatoMealsUseCase = get(),
-            getMealsForGymHelperUseCase = get(),
-            getIngredientGameUseCase = get(),
-            getSeaFoodByProteinRankUseCase = get(),
-            getMealsByDateUseCase = get(),
-            getMealsForLargeGroupUseCase = get(),
-            getMealByNameUseCase = get(),
-            soThinProblem = get(),
-            getIraqiMealsUseCase = get(),
-            getMealForKetoDietUseCase = get(),
-            getGuessGameUseCase = get ()
-        )
-    }
+    singleOf(::FoodConsoleUI)
 }
