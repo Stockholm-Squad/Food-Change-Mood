e525ed8dfa13d3cf60507559f29797e20950897d
Mona <monaayman584@gmail.com>
2025-04-22 01:27:35 +0200

    refactor get healthy fast food and use result handler and use fp

--- src/main/kotlin/logic/usecases/GetHealthyFastFoodUseCase.kt
+++ src/main/kotlin/logic/usecases/GetHealthyFastFoodUseCase.kt
@@ -1,19 +1,27 @@
 package org.example.logic.usecases
 
 import model.Meal
+import org.example.logic.model.Results
 import org.example.logic.repository.MealsRepository
 
 
 class GetHealthyFastFoodUseCase(
     private val mealsRepository: MealsRepository
 ) {
-     fun getHealthyFastFood(): List<Meal> {
-        return mealsRepository.getAllMeals().filter { it.minutes!=null&&it.minutes<=15 }
-            .sortedWith(
-                compareBy<Meal>{it.nutrition?.totalFat}
-                    .thenBy { it.nutrition?.saturatedFat }
-                    .thenBy { it.nutrition?.carbohydrates }
-            )
+    fun getHealthyFastFood(): Results<List<Meal>> {
+        return when (val meals = mealsRepository.getAllMeals()) {
+            is Results.Success -> {
+                meals.model.filter { it.minutes != null && it.minutes <= 15 }
+                    .sortedWith(
+                        compareBy<Meal> { it.nutrition?.totalFat }
+                            .thenBy { it.nutrition?.saturatedFat }
+                            .thenBy { it.nutrition?.carbohydrates }
+                    )
+                Results.Success(meals.model)
+            }
+
+            is Results.Fail -> Results.Fail(meals.exception)
+        }
     }
 }
 
--- src/main/kotlin/presentation/features/GetHealthyFastFoodMealsUI.kt
+++ src/main/kotlin/presentation/features/GetHealthyFastFoodMealsUI.kt
@@ -1,28 +1,35 @@
 package org.example.presentation.features
 
+import org.example.logic.model.Results
 import org.example.logic.usecases.GetHealthyFastFoodUseCase
 
 class GetHealthyFastFoodMealsUI(
-    private val getHealthFastFoodUseCase: GetHealthyFastFoodUseCase
+    private val getHealthyFastFoodUseCase: GetHealthyFastFoodUseCase
 ) {
 
     fun showHealthyFastFoodMeals() {
-        getHealthFastFoodUseCase.getHealthyFastFood().forEachIndexed { index, meal ->
-            println(
-                "Meal ${index + 1}: " +
-                        "Name='${meal.name}'\n " +
-                        "ID=${meal.id}\n " +
-                        "Minutes=${meal.minutes}\n " +
-                        "ContributorID=${meal.contributorId}\n " +
-                        "Submitted='${meal.submitted}\n, " +
-                        "Tags=${meal.tags}\n " +
-                        "Nutrition=${meal.nutrition}\n " +
-                        "StepsCount=${meal.numberOfSteps}\n " +
-                        "Steps=${meal.steps}\n " +
-                        "Description='${meal.description?.take(30)}...'\n " + // to avoid long prints
-                        "Ingredients=${meal.ingredients}\n " +
-                        "IngredientsCount=${meal.numberOfIngredients}\n\n"
-            )
+
+        when (val meals = getHealthyFastFoodUseCase.getHealthyFastFood()) {
+            is Results.Success -> {
+                meals.model.forEachIndexed { index, meal ->
+                    println(
+                        "Meal ${index + 1}: " +
+                                "Name='${meal.name}'\n " +
+                                "ID=${meal.id}\n " +
+                                "Minutes=${meal.minutes}\n " +
+                                "ContributorID=${meal.contributorId}\n " +
+                                "Submitted='${meal.submitted}\n, " +
+                                "Tags=${meal.tags}\n " +
+                                "Nutrition=${meal.nutrition}\n " +
+                                "StepsCount=${meal.numberOfSteps}\n " +
+                                "Steps=${meal.steps}\n " +
+                                "Description='${meal.description?.take(30)}...'\n " + // to avoid long prints
+                                "Ingredients=${meal.ingredients}\n " +
+                                "IngredientsCount=${meal.numberOfIngredients}\n\n"
+                    )
+                }
+            }
+            is Results.Fail -> println("No Meals Exist!")
         }
     }
 }
