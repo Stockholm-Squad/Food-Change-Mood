4245e1d1b2e504d33b49ab3885f008802009080a
Rodina Mo'men <Rodinamobark3@gmail.com>
2025-04-22 21:55:54 +0200

    replace custom results with Kotlin Result

--- src/main/kotlin/presentation/features/ProteinSeafoodRankingUI.kt
+++ src/main/kotlin/presentation/features/ProteinSeafoodRankingUI.kt
@@ -1,14 +1,22 @@
 package org.example.presentation.features
 
-import org.example.logic.model.Results
+import model.Meal
 import org.example.logic.usecases.GetSeaFoodByProteinRankUseCase
 
 class ProteinSeafoodRankingUI(private val getSeaFoodByProteinRankUseCase: GetSeaFoodByProteinRankUseCase) {
 
     fun proteinSeafoodRanking() {
-        when (val results = getSeaFoodByProteinRankUseCase.getSeaFoodByProteinRank()) {
-            is Results.Success -> results.model.mapIndexed { index, meal -> println("Rank: ${index + 1} Meal name: ${meal.name} Protein amount : ${meal.nutrition?.protein}") }
-            is Results.Fail -> println("No seafood meals were found in the list.h")
-        }
+        getSeaFoodByProteinRankUseCase.getSeaFoodByProteinRank().fold(
+            onSuccess = { handleSuccess(it) },
+            onFailure = { handleFailure() })
+    }
+
+    private fun handleSuccess(meals: List<Meal>) {
+        meals.mapIndexed { index, meal -> println("Rank: ${index + 1} Meal name: ${meal.name} Protein amount : ${meal.nutrition?.protein}") }
+
+    }
+
+    private fun handleFailure() {
+        println("No seafood meals were found")
     }
 }
