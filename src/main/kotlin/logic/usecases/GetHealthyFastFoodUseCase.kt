1a0d68903a7906a15411683ba835a7b49de3fbbc
Mona <monaayman584@gmail.com>
2025-04-22 17:44:09 +0200

    replace Results class with kotlin Result

--- src/main/kotlin/logic/usecases/GetHealthyFastFoodUseCase.kt
+++ src/main/kotlin/logic/usecases/GetHealthyFastFoodUseCase.kt
@@ -1,27 +1,33 @@
 package org.example.logic.usecases
 
 import model.Meal
-import org.example.logic.model.Results
 import org.example.logic.repository.MealsRepository
 
 
 class GetHealthyFastFoodUseCase(
     private val mealsRepository: MealsRepository
 ) {
-    fun getHealthyFastFood(): Results<List<Meal>> {
-        return when (val meals = mealsRepository.getAllMeals()) {
-            is Results.Success -> {
-                meals.model.filter { it.minutes != null && it.minutes <= 15 }
-                    .sortedWith(
-                        compareBy<Meal> { it.nutrition?.totalFat }
-                            .thenBy { it.nutrition?.saturatedFat }
-                            .thenBy { it.nutrition?.carbohydrates }
-                    )
-                Results.Success(meals.model)
+    fun getHealthyFastFood(): Result<List<Meal>> {
+
+        return mealsRepository.getAllMeals().fold(
+            onSuccess = { allMeals ->
+                Result.success(
+                    allMeals.filter { meal ->
+                        meal.minutes != null && meal.minutes <= 15
+                    }
+                        .sortedWith(
+                            compareBy<Meal> { it.nutrition?.totalFat }
+                                .thenBy { it.nutrition?.saturatedFat }
+                                .thenBy { it.nutrition?.carbohydrates }
+                        )
+                )
+            },
+            onFailure = { error ->
+                Result.failure(error)
             }
 
-            is Results.Fail -> Results.Fail(meals.exception)
-        }
+        )
+
     }
 }
 
--- src/main/kotlin/presentation/features/GetHealthyFastFoodMealsUI.kt
+++ src/main/kotlin/presentation/features/GetHealthyFastFoodMealsUI.kt
@@ -1,6 +1,5 @@
 package org.example.presentation.features
 
-import org.example.logic.model.Results
 import org.example.logic.usecases.GetHealthyFastFoodUseCase
 
 class GetHealthyFastFoodMealsUI(
@@ -8,28 +7,30 @@ class GetHealthyFastFoodMealsUI(
 ) {
 
     fun showHealthyFastFoodMeals() {
-
-        when (val meals = getHealthyFastFoodUseCase.getHealthyFastFood()) {
-            is Results.Success -> {
-                meals.model.forEachIndexed { index, meal ->
+        getHealthyFastFoodUseCase.getHealthyFastFood().fold(
+            onSuccess = { allMeals ->
+                allMeals.forEachIndexed { index, meal ->
                     println(
-                        "Meal ${index + 1}: " +
-                                "Name='${meal.name}'\n " +
-                                "ID=${meal.id}\n " +
-                                "Minutes=${meal.minutes}\n " +
-                                "ContributorID=${meal.contributorId}\n " +
-                                "Submitted='${meal.submitted}\n, " +
-                                "Tags=${meal.tags}\n " +
-                                "Nutrition=${meal.nutrition}\n " +
-                                "StepsCount=${meal.numberOfSteps}\n " +
-                                "Steps=${meal.steps}\n " +
-                                "Description='${meal.description?.take(30)}...'\n " + // to avoid long prints
-                                "Ingredients=${meal.ingredients}\n " +
-                                "IngredientsCount=${meal.numberOfIngredients}\n\n"
+                        "Meal ${index + 1}:\n" +
+                                "Name='${meal.name}'\n" +
+                                "ID=${meal.id}\n" +
+                                "Minutes=${meal.minutes}\n" +
+                                "ContributorID=${meal.contributorId}\n" +
+                                "Submitted='${meal.submitted}'\n" +
+                                "Tags=${meal.tags}\n" +
+                                "Nutrition=${meal.nutrition}\n" +
+                                "StepsCount=${meal.numberOfSteps}\n" +
+                                "Steps=${meal.steps}\n" +
+                                "Description='${meal.description?.take(30)}...'\n" +
+                                "Ingredients=${meal.ingredients}\n" +
+                                "IngredientsCount=${meal.numberOfIngredients}\n"
                     )
                 }
+            },
+            onFailure = { error ->
+                println("❌ Failed to load meals: ${error.message}")
             }
-            is Results.Fail -> println("No Meals Exist!")
-        }
+        )
     }
+
 }
