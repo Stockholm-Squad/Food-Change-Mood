4de709f4325a9b9f4de80f394a7366f928df9cbf
Abdelrahman Rashed <abdelrahman.rashed.ali@gmail.com>
2025-04-22 18:44:11 +0200

    updated testable refactor branch

--- src/main/kotlin/logic/usecases/GetMealForKetoDietUseCase.kt
+++ src/main/kotlin/logic/usecases/GetMealForKetoDietUseCase.kt
@@ -1,7 +1,6 @@
 package org.example.logic.usecases
 
 import model.Meal
-import org.example.logic.model.FoodChangeModeResults
 import org.example.logic.repository.MealsRepository
 
 class GetMealForKetoDietUseCase(private val mealRepository: MealsRepository) {
@@ -9,29 +8,28 @@ class GetMealForKetoDietUseCase(private val mealRepository: MealsRepository) {
     private val suggestedMeals = mutableSetOf<Int>()
 
     fun getKetoMeal(): Meal? =
-        when (val result = mealRepository.getAllMeals())  {
-            is FoodChangeModeResults.Success -> {
-                result.model.asSequence()
-                    .filter { meal ->
-                        meal.nutrition?.let { nutrition ->
-                            if (nutrition.carbohydrates != null && nutrition.totalFat != null && nutrition.protein != null) {
-                                nutrition.carbohydrates < 10 &&
-                                        nutrition.totalFat > 15 &&
-                                        nutrition.protein in 10f..30f
-                            } else {
-                                false
-                            }
-                        } == true
-                    }
-                    .filterNot { suggestedMeals.contains(it.id) }
-                    .shuffled()
-                    .firstOrNull()
-                    ?.also { suggestedMeals.add(it.id) }
+        mealRepository.getAllMeals().fold(
+            onSuccess = { meals ->
+                meals.asSequence()
+                .filter { meal ->
+                    meal.nutrition?.let { nutrition ->
+                        if (nutrition.carbohydrates != null && nutrition.totalFat != null && nutrition.protein != null) {
+                            nutrition.carbohydrates < 10 &&
+                                    nutrition.totalFat > 15 &&
+                                    nutrition.protein in 10f..30f
+                        } else {
+                            false
+                        }
+                    } == true
+                }
+                .filterNot { suggestedMeals.contains(it.id) }
+                .shuffled()
+                .firstOrNull()
+                ?.also { suggestedMeals.add(it.id) }
+            },
+            onFailure = {
+                throw it
             }
-            is FoodChangeModeResults.Fail -> {
-                throw result.exception
-            }
-        }
-
+        )
 
 }
