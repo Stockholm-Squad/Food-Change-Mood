e6d0e219747d1f8bd6863e89bf076cf3a2e4c876
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-04-22 23:14:46 +0200

    Update GetIraqiMealsUseCase.kt

--- src/main/kotlin/logic/usecases/GetIraqiMealsUseCase.kt
+++ src/main/kotlin/logic/usecases/GetIraqiMealsUseCase.kt
@@ -8,21 +8,18 @@ class GetIraqiMealsUseCase(
 ) {
     fun getIraqiMeals(): Result<List<Meal>> {
         return mealRepository.getAllMeals().fold(
-           onSuccess = { meals -> Result.success(getSuccessResult(meals))},
+           onSuccess = { meals -> getSuccessResult(meals)},
             onFailure = ::getFailureResult
         )
     }
 
-    private fun getSuccessResult(meals: List<Meal>): List<Meal> {
-        return meals.filter { meal ->
+    private fun getSuccessResult(meals: List<Meal>): Result<List<Meal>> {
+        return Result.success(meals.filter { meal ->
                     meal.description?.contains("iraq", ignoreCase = true) == true
-                            || meal.tags?.contains("iraqi") == true}
+                            || meal.tags?.contains("iraqi") == true})
         }
 
     private fun getFailureResult(throwable: Throwable): Result<List<Meal>>{
         return  Result.failure(exception = throwable)
     }
 }
-
-
-
