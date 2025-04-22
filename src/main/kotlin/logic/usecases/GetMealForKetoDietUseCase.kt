767b2285d3ed4a336fadbd70f768de514622815e
Abdelrahman Rashed <abdelrahman.rashed.ali@gmail.com>
2025-04-22 17:23:41 +0200

    refactor ketodiet to work with unit testing

--- src/main/kotlin/logic/usecases/GetMealForKetoDietUseCase.kt
+++ src/main/kotlin/logic/usecases/GetMealForKetoDietUseCase.kt
@@ -1,29 +1,37 @@
 package org.example.logic.usecases
 
 import model.Meal
+import org.example.logic.model.FoodChangeModeResults
 import org.example.logic.repository.MealsRepository
 
 class GetMealForKetoDietUseCase(private val mealRepository: MealsRepository) {
 
     private val suggestedMeals = mutableSetOf<Int>()
 
-    fun getKetoMeal(): Meal? {
-        return mealRepository.getAllMeals()
-            .asSequence()
-            .filter { meal ->
-                meal.nutrition?.let { nutrition ->
-                    if (nutrition.carbohydrates != null && nutrition.totalFat != null && nutrition.protein != null) {
-                        nutrition.carbohydrates < 10 &&
-                                nutrition.totalFat > 15 &&
-                                nutrition.protein in 10f..30f
-                    } else {
-                        false
+    fun getKetoMeal(): Meal? =
+        when (val result = mealRepository.getAllMeals())  {
+            is FoodChangeModeResults.Success -> {
+                result.model.asSequence()
+                    .filter { meal ->
+                        meal.nutrition?.let { nutrition ->
+                            if (nutrition.carbohydrates != null && nutrition.totalFat != null && nutrition.protein != null) {
+                                nutrition.carbohydrates < 10 &&
+                                        nutrition.totalFat > 15 &&
+                                        nutrition.protein in 10f..30f
+                            } else {
+                                false
+                            }
+                        } == true
                     }
-                } == true
+                    .filterNot { suggestedMeals.contains(it.id) }
+                    .shuffled()
+                    .firstOrNull()
+                    ?.also { suggestedMeals.add(it.id) }
             }
-            .filterNot { suggestedMeals.contains(it.id) }
-            .shuffled()
-            .firstOrNull()
-            ?.also { suggestedMeals.add(it.id) }
-    }
+            is FoodChangeModeResults.Fail -> {
+                throw result.exception
+            }
+        }
+
+
 }
