97311f9e01a6a68ee6dbf38f9b90f67663bcdc24
itsahmedsalah <=>
2025-04-23 09:17:05 +0200

    setup testing for country meals usecase

---
+++ src/test/kotlin/logic/usecases/GetCountryMealsUseCaseTest.kt
@@ -0,0 +1,17 @@
+package logic.usecases
+
+import org.example.logic.repository.MealsRepository
+import org.example.logic.usecases.GetCountryMealsUseCase
+import org.junit.jupiter.api.BeforeEach
+
+class GetCountryMealsUseCaseTest{
+
+  private lateinit var mealsRepository: MealsRepository
+  private lateinit var getCountryMealsUseCase : GetCountryMealsUseCase
+
+  @BeforeEach
+  fun setUp(){
+
+  }
+
+ }
