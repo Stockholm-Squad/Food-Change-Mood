248c42412ce0245a2bf7f310835c8b3321e9d9d7
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-04-23 22:52:07 +0200

    Add fun handleSuccess & fun handleFailure to handle onSuccess & onFailure

--- src/main/kotlin/logic/usecases/GetMealByNameUseCase.kt
+++ src/main/kotlin/logic/usecases/GetMealByNameUseCase.kt
@@ -10,20 +10,24 @@ class GetMealByNameUseCase(
 ) {
 
     fun getMealByName(query: String?): Result<List<Meal>> {
-        query?.takeIf { it.isNotBlank() }
-            ?: return Result.failure(Throwable(Constants.SEARCH_QUERY_CAN_NOT_BE_EMPTY))
-
-        mealsRepository.getAllMeals().onSuccess { meals ->
-            return Result.success(
-                meals
-                    .filter { searchingByKmpUseCase.searchByKmp(it.name, query) }
-                    .takeIf { it.isNotEmpty() }
-                    ?: return Result.failure(Throwable(Constants.NO_MEALS_FOUND_MATCHING))
-            )
-        }.onFailure { error ->
-            return Result.failure(Throwable("${Constants.ERROR_FETCHING_MEALS} ${error.message}"))
-        }
-
-        return Result.failure(Throwable("${Constants.UNEXPECTED_ERROR} ${this::class.simpleName}"))
+        return query?.takeIf { it.isNotBlank() }
+            ?.let { safeQuery ->
+                mealsRepository.getAllMeals().fold(
+                    onSuccess = { handleSuccess(it, safeQuery) },
+                    onFailure = ::handleFailure
+                )
+            }
+            ?: Result.failure(Throwable(Constants.SEARCH_QUERY_CAN_NOT_BE_EMPTY))
     }
+
+    private fun handleSuccess(meals: List<Meal>, query: String): Result<List<Meal>> =
+        meals
+            .filter { searchingByKmpUseCase.searchByKmp(it.name, query) }
+            .takeIf { it.isNotEmpty() }
+            ?.let { Result.success(it) }
+            ?: Result.failure(Throwable(Constants.NO_MEALS_FOUND_MATCHING))
+
+    private fun handleFailure(error: Throwable): Result<List<Meal>> =
+        Result.failure(Throwable("${Constants.ERROR_FETCHING_MEALS} ${error.message}"))
+
 }
