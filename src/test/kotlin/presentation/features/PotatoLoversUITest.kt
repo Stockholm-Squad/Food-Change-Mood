286ff22fc21c14aad2b0048248ac7b7242fd27e8
hamsa <hamssaalsaadie@gmail.com>
2025-04-23 15:32:47 +0300

    add class test for PotatoLoversUI.kt

---
+++ src/test/kotlin/presentation/features/PotatoLoversUITest.kt
@@ -0,0 +1,12 @@
+package presentation.features
+
+import org.example.logic.usecases.GetPotatoMealsUseCase
+import org.example.presentation.features.PotatoLoversUI
+import org.junit.jupiter.api.Assertions.*
+class PotatoLoversUITest(){
+
+ private lateinit var potatoMeals: GetPotatoMealsUseCase
+ private lateinit var potatoMealUi: PotatoLoversUI
+
+
+}
