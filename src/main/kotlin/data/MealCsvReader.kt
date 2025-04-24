d2c6500936fe21f086c22c2585250eb537ac1fcd
Mona <monaayman584@gmail.com>
2025-04-24 22:10:28 +0200

    add testcase for MealCsvReader

---
+++ src/main/kotlin/data/MealCsvReader.kt
@@ -0,0 +1,29 @@
+package data
+
+import org.example.data.utils.CsvLineHandler
+import java.io.File
+
+class MealCsvReader(
+    private val csvFile: File,
+    private val csvLineHandler: CsvLineHandler
+) {
+    fun readLinesFromFile(): Result<List<String>> {
+        return try {
+            val lines = mutableListOf<String>()
+            csvFile.bufferedReader().use { csvFileReader ->
+                csvFileReader.readLine()
+
+                csvFileReader.forEachLine { line ->
+                    csvLineHandler.handleLine(line)?.let { processedLine ->
+                        lines.add(processedLine)
+                    }
+                }
+            }
+            Result.success(lines)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+
+    }
+}
+
