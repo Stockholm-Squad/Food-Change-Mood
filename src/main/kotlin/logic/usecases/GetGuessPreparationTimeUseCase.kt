65b4cf2c959c1133a0165d1aacb55ecc1d2e74bb
Mona <monaayman584@gmail.com>
2025-04-24 06:10:42 +0200

    delete un needed lines

--- src/main/kotlin/logic/usecases/GetGuessPreparationTimeUseCase.kt
+++ src/main/kotlin/logic/usecases/GetGuessPreparationTimeUseCase.kt
@@ -1,12 +1,11 @@
 package org.example.logic.usecases
 
-import model.Meal
+
 import org.example.logic.usecases.model.GuessPreparationTimeState
 
 class GetGuessPreparationTimeUseCase {
     fun guessGame(
         userGuess: Int,
-        attempts: Int,
         preparationTime:Int
     ): Result<GuessPreparationTimeState> {
         return when {
@@ -19,9 +18,6 @@ class GetGuessPreparationTimeUseCase {
 
 
 
-    companion object {
-        private const val MAX_ATTEMPT = 3
-    }
 }
 
 
