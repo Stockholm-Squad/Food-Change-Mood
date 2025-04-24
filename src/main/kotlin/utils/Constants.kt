b220b9d3052b2c7181f4e494de07eaaf623ef603
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-04-24 13:36:00 +0200

    Add Constants for search meal by name

--- src/main/kotlin/utils/Constants.kt
+++ src/main/kotlin/utils/Constants.kt
@@ -7,7 +7,13 @@ object Constants {
     const val NO_MEALS_FOR_POTATO = "No potato meals found."
     const val POTATO = "potato"
     const val SEARCH_QUERY_CAN_NOT_BE_EMPTY = "Search query cannot be empty or null"
+    const val FOUND_MEALS = "Found %d meal(s):"
+    const val MEAL_DETAILS_HEADER = "Here are the details for '%s':"
+    const val SEARCH_AGAIN_PROMPT = "Would you like to search again? (y or n)"
+    const val GOODBYE_MESSAGE = "Okay! Enjoy your meals!"
     const val NO_MEALS_FOUND_MATCHING = "No meals found matching"
     const val UNEXPECTED_ERROR = "Unexpected error occurred"
     const val ERROR_FETCHING_MEALS = "Error fetching meals: "
+    const val INVALID_SELECTION_MESSAGE = "Invalid selection. Please choose a valid number."
+
 }
