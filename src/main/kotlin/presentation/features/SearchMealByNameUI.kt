df0578a68860f88adf8c2f28bd1f547ad8696762
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-04-24 13:39:50 +0200

    Add Constants & use printer & reader interfaces at SearchMealByNameUI.kt

--- src/main/kotlin/presentation/features/SearchMealByNameUI.kt
+++ src/main/kotlin/presentation/features/SearchMealByNameUI.kt
@@ -2,58 +2,63 @@ package org.example.presentation.features
 
 import org.example.logic.usecases.GetMealByNameUseCase
 import model.Meal
+import org.example.input_output.input.InputReader
+import org.example.input_output.output.OutputPrinter
+import org.example.utils.Constants
 
-class SearchMealByNameUI(private val getMealByNameUseCase: GetMealByNameUseCase) {
+class SearchMealByNameUI(
+    private val getMealByNameUseCase: GetMealByNameUseCase,
+    private val reader: InputReader,
+    private val printer: OutputPrinter
+) {
 
     fun handleSearchByName() {
-        print("🔍 Enter a meal keyword to search: ")
-        readlnOrNull()?.trim()
+        printer.printLine("🔍 Enter a meal keyword to search: ")
+        reader.readLineOrNull()?.trim()
             ?.takeIf { it.isNotBlank() }
-            ?.let { query ->
-                getMealByNameUseCase.getMealByName(query)
+            ?.let { pattern ->
+                getMealByNameUseCase.getMealByName(pattern)
                     .onSuccess(::displayMeals)
-                    .onFailure { println("❌ ${it.message}") }
+                    .onFailure { printer.printLine(it.message) }
             }
-            ?: println("❌ Search query cannot be empty.")
+            ?: printer.printLine(Constants.SEARCH_QUERY_CAN_NOT_BE_EMPTY)
 
         askForMoreMeals()
     }
 
     private fun displayMeals(meals: List<Meal>) {
-        if (meals.isEmpty()) {
-            println("❌ No meals found matching the query.")
-        } else {
-            println("✅ Found ${meals.size} meal(s):")
-            meals.forEachIndexed { index, meal -> println("${index + 1}. ${meal.name}") }
-            askToViewMealDetails(meals)
-        }
+        meals.takeIf { it.isNotEmpty() }?.let {
+            printer.printLine(Constants.FOUND_MEALS.format(it.size))
+            it.forEachIndexed { index, meal -> println("${index + 1}. ${meal.name}") }
+            askToViewMealDetails(it)
+        } ?: printer.printLine(Constants.NO_MEALS_FOUND_MATCHING)
     }
 
+
     private fun askToViewMealDetails(meals: List<Meal>) {
-        println("\nWould you like to view the details of any of these meals? (Enter the number or 'n' to skip):")
-        readlnOrNull()?.trim()?.lowercase()
+        printer.printLine("\nWould you like to view the details of any of these meals? (Enter the number or 'n' to skip):")
+        reader.readLineOrNull()?.trim()?.lowercase()
             ?.takeIf { it != "n" }
             ?.let { input ->
                 input.toIntOrNull()
                     ?.takeIf { it in 1..meals.size }
                     ?.let { showMealDetails(meals[it - 1]) }
-                    ?: println("❌ Invalid selection. Please choose a valid number.")
+                    ?: printer.printLine(Constants.INVALID_SELECTION_MESSAGE)
             }
     }
 
     private fun showMealDetails(meal: Meal) {
-        println("\nHere are the details for '${meal.name}':")
-        println(meal)
+        printer.printLine(Constants.MEAL_DETAILS_HEADER.format(meal.name))
+        printer.printMeal(meal)
     }
 
     private fun askForMoreMeals() {
-        println(
-            "------------------------------------------------\n" +
-                    "Would you like to search again? (y or n)"
-        )
-        readlnOrNull()?.trim()?.lowercase()
+        printer.printLine(Constants.SEARCH_AGAIN_PROMPT)
+        reader.readLineOrNull()?.trim()?.lowercase()
             ?.takeIf { it == "y" }
-            ?.let { handleSearchByName() }
-            ?: println("Okay! Enjoy your meals! 😋")
+            ?.let {
+                handleSearchByName()
+            }
+            ?: printer.printLine(Constants.GOODBYE_MESSAGE)
     }
 }
