f88444b0bd0ec4ace82ed51b2ad0df9401093feb
Thoraya <thoriahamdy99@gmail.com>
2025-04-25 21:29:50 +0300

    add missing columns in csv data.

--- src/test/kotlin/utils/ParserDataBuilder.kt
+++ src/test/kotlin/utils/ParserDataBuilder.kt
@@ -23,26 +23,19 @@ const val mealAsString = "\"mixecan meal,137739,55,47892,2005-09-16\"," +
         "7"
 
 val mealAsListOfStrings = listOf(
-    "mixecan meal", "137739", "55", "47892", "2005-09-16",
+            "mixecan meal",
+        "137739",
+        "55",
+        "47892",
+        "2005-09-16",
+        "['60-minutes-or-less', 'time-to-make', 'course', 'main-ingredient', 'cuisine', 'preparation', 'occasion', 'north-american', 'side-dishes', 'vegetables', 'mexican', 'easy', 'fall', 'holiday-event', 'vegetarian', 'winter', 'dietary', 'christmas', 'seasonal', 'squash']",
+        "[51.5, 0.0, 13.0, 0.0, 2.0, 0.0, 4.0]",
+        "11",
+        "['make a choice and proceed with recipe', 'depending on size of squash , cut into half or fourths', 'remove seeds', 'for spicy squash , drizzle olive oil or melted butter over each cut squash piece', 'season with mexican seasoning mix ii', 'for sweet squash , drizzle melted honey , butter , grated piloncillo over each cut squash piece', 'season with sweet mexican spice mix', 'bake at 350 degrees , again depending on size , for 40 minutes up to an hour , until a fork can easily pierce the skin', 'be careful not to burn the squash especially if you opt to use sugar or butter', 'if you feel more comfortable , cover the squash with aluminum foil the first half hour , give or take , of baking', 'if desired , season with salt']",
+        "autumn is my favorite time of year to cook! this recipe \n can be prepared either spicy or sweet, your choice!two of my posted mexican-inspired seasoning mix recipes are offered as suggestions.",
+        "['winter squash', 'mexican seasoning', 'mixed spice', 'honey', 'butter', 'olive oil', 'salt']",
+        "7"
 
-    "['60-minutes-or-less', 'time-to-make', 'course', 'main-ingredient', 'cuisine', 'preparation'," +
-            " 'occasion', 'north-american', 'side-dishes', 'vegetables', 'mexican', 'easy', 'fall'," +
-            " 'holiday-event', 'vegetarian', 'winter', 'dietary', 'christmas', 'seasonal', 'squash']",
-
-    "['make a choice and proceed with recipe', 'depending on size of squash , cut into half or fourths'," +
-            " 'remove seeds', 'for spicy squash , drizzle olive oil or melted butter over each cut squash piece', " +
-            "'season with mexican seasoning mix ii', 'for sweet squash , drizzle melted honey , butter , grated piloncillo over each cut squash piece', " +
-            "'season with sweet mexican spice mix', 'bake at 350 degrees , again depending on size , for 40 minutes up to an hour , until a fork can easily pierce the skin', " +
-            "'be careful not to burn the squash especially if you opt to use sugar or butter'," +
-            " 'if you feel more comfortable , cover the squash with aluminum foil the first half hour , give or take , of baking', " +
-            "'if desired , season with salt']",
-
-    "autumn is my favorite time of year to cook! this recipe \n can be prepared either spicy or sweet, your choice!" +
-            "two of my posted mexican-inspired seasoning mix recipes are offered as suggestions.",
-
-    "['winter squash', 'mexican seasoning', 'mixed spice', 'honey', 'butter', 'olive oil', 'salt']",
-
-    "7"
 )
 
 val listOfMealsAsString = listOf(
